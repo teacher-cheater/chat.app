e74809b1b3a417c167deb24b06530f2f188bf79e
teacher_cheater <johnnykayfun@gmail.com>
2022-08-19 13:58:41 +0300

    deploy project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="wrapper">
-      <BrowserRouter >
+      <BrowserRouter basename="chat.app">
         <Routes>
           <Route
             path="/"
